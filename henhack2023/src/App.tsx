baeb0a99f5209de67716ce4e9025c14ab859a8d1
cthacker@udel.edu <cthacker@udel.edu>
2023-04-29 15:00:16 -0400

    [main] Added instructions modal and home screen

--- henhack2023/src/App.tsx
+++ henhack2023/src/App.tsx
@@ -20,11 +20,28 @@ function App(): JSX.Element {
   });
 
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&family=Righteous&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+          crossOrigin="anonymous"
+        />
+      </head>
       {displayScreen.home ? <HomeScreen /> : null}
-    </div>
-  )
-};
+    </>
+  );
+}
 
-export default App
+export default App;
--- henhack2023/src/components/HomeScreen/HomeScreen.tsx
+++ henhack2023/src/components/HomeScreen/HomeScreen.tsx
@@ -1,5 +1,47 @@
 import React from "react";
+import { Button, Modal } from "react-bootstrap";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from "src/styles/HomeScreen.module.css";
 
-export const HomeScreen = (): JSX.Element => <div>{"Hello"}</div>;
+export const HomeScreen = (): JSX.Element => {
+  const [showInstructions, setShowInstructions] =
+    React.useState<boolean>(false);
+
+  const toggleShowInstructions = React.useCallback(() => {
+    setShowInstructions((oldShowInstructions: boolean) => !oldShowInstructions);
+  }, []);
+
+  return (
+    <>
+      <div className={styles.home_screen}>
+        <div className={styles.home_screen_upper_content}>
+          <div className={styles.home_screen_title}>
+            {"Splitting Suspects: A Recursive Bank Heist Mystery"}
+          </div>
+          <div className={styles.home_screen_content}>
+            <div className={styles.home_screen_description}>
+              {
+                "Put your facial recognition skills to the test and solve the bank robbery with recursive precision!"
+              }
+            </div>
+            <div className={styles.home_screen_action_buttons}>
+              <Button
+                onClick={toggleShowInstructions}
+                variant="outline-secondary"
+              >
+                {"Instructions"}
+              </Button>
+              <Button variant="outline-success">{"Start Investigation"}</Button>
+            </div>
+          </div>
+        </div>
+        <div className={styles.home_screen_footer}>{"Created by BTBB"}</div>
+      </div>
+      <Modal show={showInstructions} onHide={toggleShowInstructions}>
+        <Modal.Header>{"Instructions"}</Modal.Header>
+        <Modal.Body>{"Instructions content"}</Modal.Body>
+        <Modal.Footer>{"Instructions footer"}</Modal.Footer>
+      </Modal>
+    </>
+  );
+};
