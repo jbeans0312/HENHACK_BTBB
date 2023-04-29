7d269e876750ecc01822d1fd96f38c331bc554d0
jbeans0312 <jbeans@udel.edu>
2023-04-29 15:00:55 -0400

    beginner warmup screen

---
+++ henhack2023/src/components/WarmupScreen/DraggableSuspect.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useDrag } from 'react-dnd';
+import "../styles/WarmupScreen"
+
+export function DraggableSuspect({ id }: { id: string }): JSX.Element {
+  const [{ isDragging }, dragRef] = useDrag({
+    type: 'image',
+    item: { id },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging()
+    }),
+  })
+  return (
+    <img className='susImage' ref={dragRef} src={require('../suspect_images/suspect_' + id + '.png')}>
+        {isDragging}
+    </img>
+  )
+}
