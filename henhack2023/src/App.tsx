90339c93ccab631a4d6d1707598123084e24e8b2
cthacker@udel.edu <cthacker@udel.edu>
2023-04-29 17:26:35 -0400

    [main] One more

--- henhack2023/src/App.tsx
+++ henhack2023/src/App.tsx
@@ -6,6 +6,8 @@ import { HomeScreen } from "./components";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { WarmupScreen } from "./components/WarmupScreen/WarmupScreen";
 import { DndProvider } from "react-dnd";
+import { SuspectCard } from "./components/SuspectCard";
+
 /**
  *
  */
@@ -14,6 +16,8 @@ export type ScreenDisplay = {
   game: boolean;
   warmup: boolean;
 } & Record<string, boolean>;
+
+const IMAGES = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
 
 function App(): JSX.Element {
   /**
@@ -44,32 +48,23 @@ function App(): JSX.Element {
   return (
     <DndProvider backend={HTML5Backend}>
       <>
-        <head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin=""
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&family=Righteous&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-            integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
-            crossOrigin="anonymous"
-          />
-        </head>
         {displayScreen.home ? (
           <HomeScreen
             toggleShowGame={() => {
-              changeDisplay("game");
+              changeDisplay("warmup");
             }}
           />
         ) : null}
-        {displayScreen.warmup ? <div /> : null}
+        {displayScreen.warmup ? (
+          <>
+            {IMAGES.map((eachSuspectName: string, index: number) => (
+              <SuspectCard
+                key={`suspect_${index}_${eachSuspectName}`}
+                id={eachSuspectName}
+              />
+            ))}
+          </>
+        ) : null}
         {displayScreen.game ? <div /> : null}
       </>
     </DndProvider>
--- henhack2023/src/index.tsx
+++ henhack2023/src/index.tsx
@@ -7,7 +7,27 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <React.StrictMode>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&family=Righteous&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+          crossOrigin="anonymous"
+        />
+      </head>
+      <App />
+    </React.StrictMode>
+  </>
 );
