0a6cc637af959173e4fe00068ead180bbfb30c0b
Craig Barber <RacgiMan@icloud.com>
2023-04-29 17:27:43 -0400

    idk why it's saying I changed App.tsx

--- henhack2023/src/App.tsx
+++ henhack2023/src/App.tsx
@@ -48,29 +48,6 @@ function App(): JSX.Element {
 
   return (
     <DndProvider backend={HTML5Backend}>
-<<<<<<< HEAD
-    <>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin=""
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&family=Righteous&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
-          crossOrigin="anonymous"
-        />
-      </head>
-      <WarmupScreen></WarmupScreen>
-    </>
-=======
       <>
         {displayScreen.home ? (
           <HomeScreen
@@ -91,7 +68,6 @@ function App(): JSX.Element {
         ) : null}
         {displayScreen.game ? <div /> : null}
       </>
->>>>>>> 9187b1e86eec9d06e7cf08397df880910071bef5
     </DndProvider>
   );
 }
