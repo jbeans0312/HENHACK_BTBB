597e905edb69353e8ea267bb652ea95f1244d675
cthacker-udel <cthacker@udel.edu>
2023-04-30 03:26:14 -0400

    [main] Added fade-in animations to title screen

--- henhack2023/src/components/HomeScreen/HomeScreen.tsx
+++ henhack2023/src/components/HomeScreen/HomeScreen.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import logo from "../../icon.png";
-import { Button, Modal, Image } from "react-bootstrap";
+import { Button, Modal, Image, Fade } from "react-bootstrap";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from "src/styles/HomeScreen.module.css";
+import FadeIn from "react-fade-in/lib/FadeIn";
 
 type HomeScreenProperties = {
   toggleShowGame: () => void;
@@ -17,11 +18,16 @@ type HomeScreenProperties = {
 export const HomeScreen = ({
   toggleShowGame,
 }: HomeScreenProperties): JSX.Element => {
+  const [showTitle, setShowTitle] = React.useState<boolean>(false);
   const [showInstructions, setShowInstructions] =
     React.useState<boolean>(false);
 
   const toggleShowInstructions = React.useCallback(() => {
     setShowInstructions((oldShowInstructions: boolean) => !oldShowInstructions);
+  }, []);
+
+  React.useEffect(() => {
+    setShowTitle(true);
   }, []);
 
   return (
@@ -44,32 +50,36 @@ export const HomeScreen = ({
             />
           </div>
 
-          <div className={styles.home_screen_title}>
-            {"Splitting Suspects: A Recursive Bank Heist Mystery"}
-          </div>
-          <div className={styles.home_screen_content}>
-            <div className={styles.home_screen_description}>
-              {
-                "Put your facial recognition skills to the test and solve the bank robbery with recursive precision!"
-              }
+          <FadeIn transitionDuration={700}>
+            <div className={styles.home_screen_title}>
+              {"Splitting Suspects: A Recursive Bank Heist Mystery"}
             </div>
-            <div className={styles.home_screen_action_buttons}>
-              <Button
-                onClick={toggleShowInstructions}
-                variant="outline-secondary"
-              >
-                {"Instructions"}
-              </Button>
-              <Button
-                variant="outline-success"
-                onClick={() => {
-                  toggleShowGame();
-                }}
-              >
-                {"Start Investigation"}
-              </Button>
+            <div className={styles.home_screen_content}>
+              <div className={styles.home_screen_description}>
+                {
+                  "Put your facial recognition skills to the test and solve the bank robbery with recursive precision!"
+                }
+              </div>
+              <div className={styles.home_screen_action_buttons}>
+                <Button
+                  className={styles.home_screen_button}
+                  onClick={toggleShowInstructions}
+                  variant="outline-secondary"
+                >
+                  {"Instructions"}
+                </Button>
+                <Button
+                  className={styles.home_screen_button}
+                  variant="outline-success"
+                  onClick={() => {
+                    toggleShowGame();
+                  }}
+                >
+                  {"Start Investigation"}
+                </Button>
+              </div>
             </div>
-          </div>
+          </FadeIn>
         </div>
         <div className={styles.home_screen_footer}>{"Created by BTBB"}</div>
       </div>
@@ -79,18 +89,20 @@ export const HomeScreen = ({
         show={showInstructions}
         onHide={toggleShowInstructions}
       >
-        <Modal.Header
-          className={styles.home_screen_instructions_header}
-          closeButton
-          closeVariant="white"
-        >
-          {"Instructions"}
-        </Modal.Header>
-        <Modal.Body className={styles.home_screen_instructions_body}>
-          {
-            "In this detective game, players must use clues about a suspect's face to determine if they robbed a bank. Using recursion, players will divide suspects into smaller groups based on facial characteristics until the perpetrator is identified."
-          }
-        </Modal.Body>
+        <FadeIn transitionDuration={700}>
+          <Modal.Header
+            className={styles.home_screen_instructions_header}
+            closeButton
+            closeVariant="white"
+          >
+            {"Instructions"}
+          </Modal.Header>
+          <Modal.Body className={styles.home_screen_instructions_body}>
+            {
+              "In this detective game, players must use clues about a suspect's face to determine if they robbed a bank. Using recursion, players will divide suspects into smaller groups based on facial characteristics until the perpetrator is identified."
+            }
+          </Modal.Body>
+        </FadeIn>
       </Modal>
     </>
   );
